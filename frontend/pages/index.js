e3ac0b38db9eb1ca89d17495f5a202dd7365a16b
davejean177-jpg <davejean177@gmail.com>
2026-03-22 00:28:30 -0400

    Initial Next.js Home Page

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SensitivityForm from '../components/SensitivityForm';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Free Fire Sensitivity Generator</h1>
+      <SensitivityForm />
+    </div>
+  );
+};
+
+export default Home;
